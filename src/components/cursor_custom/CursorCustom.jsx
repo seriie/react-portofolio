0c0180425a7adbb70796861ae878950e4d322447
serrie <mohammadzidane058@gmail.com>
2024-11-21 16:43:07 +0700

    fix cursor custom

--- src/components/cursor_custom/CursorCustom.jsx
+++ src/components/cursor_custom/CursorCustom.jsx
@@ -10,31 +10,32 @@ export default function CursorCustom() {
         const cursor = document.querySelector('.cursor');
         const dot = document.querySelector('.cursor .dot');
         const circle = document.querySelector('.cursor .circle');
-
-        document.addEventListener('mousemove', function(event) {
-            dot.style.top = `${event.pageY}px`;
-            dot.style.left = `${event.pageX}px`;
-            circle.style.top = `${event.pageY}px`;
-            circle.style.left = `${event.pageX}px`;
-        }); 
-
-        document.addEventListener('mouseleave', function() {
-            cursor.style.display = "none";
-        });
-        
-        document.addEventListener('mousemove', function() {
-            cursor.style.display = "block";
-        });
-
-        triggeredHover.forEach(triggered => {
-            triggered.addEventListener('mouseenter', function() {
-                setIsTriggered(true);
+        if(window.innerWidth > 700) {
+            document.addEventListener('mousemove', function(event) {
+                dot.style.top = `${event.pageY}px`;
+                dot.style.left = `${event.pageX}px`;
+                circle.style.top = `${event.pageY}px`;
+                circle.style.left = `${event.pageX}px`;
+            }); 
+    
+            document.addEventListener('mouseleave', function() {
+                cursor.style.display = "none";
             });
-
-            triggered.addEventListener('mouseleave', function() {
-                setIsTriggered(false);
+            
+            document.addEventListener('mousemove', function() {
+                cursor.style.display = "block";
             });
-        });
+    
+            triggeredHover.forEach(triggered => {
+                triggered.addEventListener('mouseenter', function() {
+                    setIsTriggered(true);
+                });
+    
+                triggered.addEventListener('mouseleave', function() {
+                    setIsTriggered(false);
+                });
+            });
+        }
     });
 
     return (
